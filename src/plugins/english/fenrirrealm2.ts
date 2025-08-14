85af9ffc3a2066e8c996cff9ffe9c6bcbb9b74df
malpyon <m.alfindo@gmail.com>
2025-08-14 12:39:02 +0700

    add my fenrir2

--- src/plugins/english/fenrirrealm2.ts
+++ src/plugins/english/fenrirrealm2.ts
@@ -5,11 +5,11 @@ import { Filters, FilterTypes } from '@libs/filterInputs';
 import { storage } from '@libs/storage';
 
 class FenrirRealmPlugin implements Plugin.PluginBase {
-  id = 'fenrir';
-  name = 'Fenrir Realm';
+  id = 'fenrir2';
+  name = 'Fenrir Realm2';
   icon = 'src/en/fenrirrealm/icon.png';
   site = 'https://fenrirealm.com';
-  version = '1.0.8';
+  version = '1.0.9';
   imageRequestInit?: Plugin.ImageRequestInit | undefined = undefined;
 
   hideLocked = storage.get('hideLocked');
